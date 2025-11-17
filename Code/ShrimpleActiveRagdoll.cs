ffc8f98d383c53043ad70a9314af5c71ffa2024b
yuberee <59583743+yuberee@users.noreply.github.com>
2025-11-17 01:21:54 +0100

    Implement RagdollFollowMode

--- Code/ShrimpleActiveRagdoll.cs
+++ Code/ShrimpleActiveRagdoll.cs
@@ -69,8 +69,12 @@
 		}
 	}
 
+	/// <summary>
+	/// If the ragdoll's renderer is not the root object, how should the root gameobject follow the ragdoll's movement<br />
+	/// Useful for characters where the renderer is a child of the main object<br />
+	/// If you want to move the ragdoll you'll have to move the <see cref="Renderer"/>'s GameObject
+	/// </summary>
 	[Property]
-	[ShowIf( nameof( Mode ), RagdollMode.Enabled )]
 	public RagdollFollowMode FollowMode
 	{
 		get;
@@ -126,8 +130,19 @@
 			MoveMeshFromBodies();
 		}
 		if ( Mode == RagdollMode.Statue )
-		{
 			MoveMeshFromObjects();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		if ( Renderer.GameObject != GameObject.Root )
+		{
+			if ( FollowMode.Contains( RagdollFollowMode.Position ) )
+				GameObject.Root.WorldPosition = Renderer.GameObject.WorldPosition;
+			if ( FollowMode.Contains( RagdollFollowMode.Rotation ) )
+				GameObject.Root.WorldRotation = Renderer.GameObject.WorldRotation;
 		}
 	}
 
