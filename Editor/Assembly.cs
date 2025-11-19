328ff0df9c1ab40629f27ef38a82b13ecea8c1df
yuberee <59583743+yuberee@users.noreply.github.com>
2025-11-20 00:02:57 +0100

    assembly and editor assembly

--- Editor/Assembly.cs
+++ Editor/Assembly.cs
@@ -1,1 +1,9 @@
 global using Editor;
+global using Editor.NodeEditor;
+global using Sandbox;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+
+
+namespace ShrimpleActiveRagdolls.Editor;
