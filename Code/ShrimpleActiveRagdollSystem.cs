4f747dd5e73a37b5dc2afa0726c52983b60a1842
yuberee <59583743+yuberee@users.noreply.github.com>
2025-11-30 23:40:46 +0100

    ShrimpleActiveRagdollSystem

---
+++ Code/ShrimpleActiveRagdollSystem.cs
@@ -0,0 +1,24 @@
+﻿public class ShrimpleActiveRagdollSystem : GameObjectSystem
+{
+	public ShrimpleActiveRagdollSystem( Scene scene ) : base( scene )
+	{
+		Listen( Stage.UpdateBones, 65, UpdateVisuals, "Updating Ragdoll Visuals" );
+		Listen( Stage.PhysicsStep, 65, UpdatePhysics, "Updating Ragdoll Physics" );
+	}
+
+	void UpdateVisuals()
+	{
+		var allRagdolls = Scene.GetAllComponents<ShrimpleActiveRagdoll>();
+
+		foreach ( var ragdoll in allRagdolls )
+			ragdoll.ComputeVisuals();
+	}
+
+	void UpdatePhysics()
+	{
+		var allRagdolls = Scene.GetAllComponents<ShrimpleActiveRagdoll>();
+
+		foreach ( var ragdoll in allRagdolls )
+			ragdoll.ComputePhysics();
+	}
+}
