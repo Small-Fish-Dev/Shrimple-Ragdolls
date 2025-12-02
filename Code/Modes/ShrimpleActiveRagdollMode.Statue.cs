4a5ab315dedfc522c1b0f50f624ad6544630ddf3
yuberee <59583743+yuberee@users.noreply.github.com>
2025-12-02 21:24:54 +0100

    ShrimpleActiveRagdollMode

---
+++ Code/Modes/ShrimpleActiveRagdollMode.Statue.cs
@@ -0,0 +1,19 @@
+﻿public class ShrimpleActiveRagdollStatue : ShrimpleActiveRagdollMode
+{
+	public override void OnEnter( ShrimpleActiveRagdoll ragdoll )
+	{
+
+	}
+	public override void OnExit( ShrimpleActiveRagdoll ragdoll )
+	{
+
+	}
+	public override void PhysicsUpdate( ShrimpleActiveRagdoll ragdoll )
+	{
+
+	}
+	public override void VisualUpdate( ShrimpleActiveRagdoll ragdoll )
+	{
+
+	}
+}
---
+++ Code/Modes/ShrimpleActiveRagdollMode.cs
@@ -0,0 +1,7 @@
+﻿public abstract class ShrimpleActiveRagdollMode
+{
+	public abstract void OnEnter( ShrimpleActiveRagdoll ragdoll );
+	public abstract void OnExit( ShrimpleActiveRagdoll ragdoll );
+	public abstract void PhysicsUpdate( ShrimpleActiveRagdoll ragdoll );
+	public abstract void VisualUpdate( ShrimpleActiveRagdoll ragdoll );
+}
