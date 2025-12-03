e25afb88c0ddcc17ff5e3b763786f30a231f0713
yuberee <59583743+yuberee@users.noreply.github.com>
2025-12-03 14:36:33 +0100

    Use ClearBoneOverrides for now

--- Code/Modes/ShrimpleRagdollMode.Disabled.cs
+++ Code/Modes/ShrimpleRagdollMode.Disabled.cs
@@ -13,7 +13,7 @@
 			ragdoll.MakeRendererAbsolute( false ); // Remove absolute from model if we're root
 
 		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone ); // Remove tags
-		ragdoll.Renderer.SceneModel.SetBoneOverride( body.BoneIndex, Transform.Zero ); // Try to reset bone override this way, if not we'll need to call ClearBoneOverrides
+		ragdoll.Renderer.SceneModel.ClearBoneOverrides(); // TODO: Make this only ClearBoneOverride( boneIndex ) once feature request gets accepte
 	}
 
 	public static void OnExit( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
