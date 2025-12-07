fb31695ff4f89a65efd566cad033725b7ee574aa
yuberee <59583743+yuberee@users.noreply.github.com>
2025-12-08 00:43:57 +0100

    New mode: ActiveMode

---
+++ Code/Modes/ShrimpleRagdollMode.Active.cs
@@ -0,0 +1,43 @@
+﻿public class ActiveMode : IShrimpleRagdollMode<ActiveMode>
+{
+	public static string Name => "Active";
+	public static string Description => "Bodies will physically move where they're meant to be, unless blocked";
+	public static bool PhysicsDriven => false; // We follow the renderer's animations!
+
+	public static void OnEnter( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
+	{
+		body.EnableColliders();
+		body.Component?.Enabled = true;
+		body.Component?.Gravity = false; // Gravity's hard to combat when we want it to be as close to the animations as possible while not using crazy amounts of force!
+		ragdoll.GetParentJoint( body )?.Component.Enabled = true;
+
+		if ( body.IsRootBone )
+			ragdoll.MakeRendererAbsolute( true ); // Make model absolute if we're root
+
+		ragdoll.AddFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone );
+		ragdoll.MoveObjectFromMesh( body.GetBone( ragdoll.Model ) );
+	}
+
+	public static void OnExit( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
+	{
+		if ( body.IsRootBone )
+			ragdoll.MakeRendererAbsolute( false ); // Remove absolute from model if we're root
+		body.Component?.Gravity = false;
+		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone );
+	}
+
+	public static void PhysicsUpdate( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
+	{
+		ragdoll.MoveBodyFromAnimations( body );
+	}
+
+	public static void VisualUpdate( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
+	{
+		ragdoll.MoveMeshFromObject( body );
+	}
+
+	static ActiveMode()
+	{
+		ShrimpleRagdollModeRegistry.Register<ActiveMode>();
+	}
+}
