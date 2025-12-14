0630ce64ce3037cac92e84bf446eb4568c5faa61
yuberee <59583743+yuberee@users.noreply.github.com>
2025-12-14 14:14:47 +0100

    Statue Mode employs ProceduralBone flag (Maybe do to everyone?)

--- Code/Modes/StatueMode.cs
+++ Code/Modes/StatueMode.cs
@@ -20,9 +20,9 @@
 			ragdoll.SetupPhysics(); // We created a new rigidbody, so we gotta give it all the settings we have
 		}
 
-		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone );
 		ragdoll.MoveObjectFromMesh( body.GetBone() );
 		ragdoll.MoveMeshFromObject( body );
+		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone );
 	}
 
 	public static void OnExit( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
@@ -32,6 +32,7 @@
 			ragdoll.MakeRendererAbsolute( false );
 			ragdoll.Renderer.GetComponent<Rigidbody>( true )?.Enabled = false;
 		}
+		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.ProceduralBone );
 	}
 
 	public static void PhysicsUpdate( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
@@ -40,6 +41,9 @@
 
 	public static void VisualUpdate( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
 	{
+		if ( !body.GameObject.Flags.HasFlag( GameObjectFlags.ProceduralBone ) ) // For some reason it's delayed when done inside of OnEnter? So we do it here even if not optimal
+			ragdoll.AddFlags( body.GameObject, GameObjectFlags.ProceduralBone ); // Add procedural bone so it's ignored by the boneObjectList updates in SkinnedModelRenderer
+
 		ragdoll.MoveMeshFromObject( body );
 	}
 
