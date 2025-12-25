9e207c695c6439b8188b381cc9997aa786bd2323
yuberee <59583743+yuberee@users.noreply.github.com>
2025-12-26 00:53:37 +0100

    Fix NRE

--- Code/Modes/ActiveMode.cs
+++ Code/Modes/ActiveMode.cs
@@ -23,7 +23,10 @@ public class ActiveMode : IShrimpleRagdollMode<ActiveMode>
 	public static void OnExit( ShrimpleRagdoll ragdoll, ShrimpleRagdoll.Body body )
 	{
 		body.Component?.Gravity = true;
-		ragdoll.ResetJointSettings( body.GetParentJoint().Value );
+
+		var parentJoint = body.GetParentJoint();
+		if ( parentJoint != null )
+			ragdoll.ResetJointSettings( body.GetParentJoint().Value );
 		ragdoll.RemoveFlags( body.GameObject, GameObjectFlags.Absolute | GameObjectFlags.PhysicsBone );
 	}
 
