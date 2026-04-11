c3dfbdfbb17c0b7579e8d5a4e38e6f9e479848b5
yuberee <59583743+yuberee@users.noreply.github.com>
2026-04-11 21:40:44 +0200

    Oops forgot the shrimpleragdollsystem

---
+++ Code/ShrimpleRagdollSystem.cs
@@ -0,0 +1,18 @@
+﻿namespace ShrimpleRagdolls;
+
+public class ShrimpleRagdollSystem : GameObjectSystem
+{
+	public ShrimpleRagdollSystem( Scene scene ) : base( scene )
+	{
+		Listen( Stage.UpdateBones, 0, UpdateHitReactions, "Updating Ragdoll Hit Reactions" );
+	}
+
+	void UpdateHitReactions()
+	{
+		foreach ( var ragdoll in Scene.GetAllComponents<ShrimpleRagdoll>() )
+		{
+			if ( ragdoll.IsValid() )
+				ragdoll.UpdateHitReactions();
+		}
+	}
+}
